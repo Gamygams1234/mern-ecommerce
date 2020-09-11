35192a0d428ec21957ca933a73856d0bf905078d
Gamygams1234 <gamyburgos@gmail.com>
2020-09-11 01:12:10 -0700

    Addinf the virtual fields

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
-const { stringify } = require("querystring");
+const uuidv1 = require("uuid/v1");
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +17,34 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
+// virtual field
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  // this will encript our password
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      // this is how we encrypt
+      return crypto.createHmac("sha1", this.salt).update(password).digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
