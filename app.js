e88a3f27621b3d6ff980789625068f9bf82cb898
Gamygams1234 <gamyburgos@gmail.com>
2020-09-10 23:02:21 -0700

    Connecting to MONGO DB

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 require("dotenv").config();
+
+// db
+
+mongoose
+  .connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+  .then(console.log("MongoDB Connected..."))
+  .catch((error) => {
+    console.error(error);
+  });
 
 app.get("/", (req, res) => {
   res.send("If she sees my stscks");
