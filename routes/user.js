1f4521ca54d930de406ecc6ffd61ac33fb9db529
Gamygams1234 <gamyburgos@gmail.com>
2020-09-13 02:07:41 -0700

    Checcking for  admin and checking for authentication

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { requireSignin } = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 // create method
 
-router.get("/secret/:userId", requireSignin, (req, res) => {
+// putting our middleware to check if it is admin and if it is authenicated
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile,
   });
